fca99acb2687c4ae3e3c2c9e0f6090091d5b1afb
krstnnnnn <kirstencastro43@gmail.com>
2026-09-10 07:07:30 +0800

    laboratory 5

--- app/config/config.php
+++ app/config/config.php
@@ -350,4 +350,13 @@ $config['csrf_cookie_name']        = 'csrf_cookie_name';
 $config['csrf_expire']             = 7200;
 $config['csrf_regenerate']         = FALSE;
 
+
+
+   require_once APP_DIR . 'middlewares/StudentMiddleware.php';
+   require_once APP_DIR . 'middlewares/AuthMiddleware.php';
+
+   $config['middlewares'] = [
+       'StudentMiddleware' => new StudentMiddleware(),
+       'auth_check'        => new AuthMiddleware(),
+   ];
 ?>
