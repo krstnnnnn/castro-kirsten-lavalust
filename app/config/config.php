1db0ec02e77a212830e55162ecd7e1cacb87aefb
krstnnnnn <kirstencastro43@gmail.com>
2026-09-10 07:23:48 +0800

    laboratory 5

--- app/config/config.php
+++ app/config/config.php
@@ -356,7 +356,7 @@ $config['csrf_regenerate']         = FALSE;
    require_once APP_DIR . 'middlewares/AuthMiddleware.php';
 
    $config['middlewares'] = [
-       'StudentMiddleware' => new StudentMiddleware(),
-       'auth_check'        => new AuthMiddleware(),
-   ];
+    'StudentMiddleware' => new StudentMiddleware(),
+    'AuthMiddleware'    => new AuthMiddleware(),
+];
 ?>
